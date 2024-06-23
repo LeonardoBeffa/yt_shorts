94dcc9184cd497e0244c700557f612cd86808e2a
LeonardoBeffa <leonardorbeffa@gmail.com>
2024-06-23 17:39:45 -0300

    main

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from yt_video_transcript import get_transcript, download_video
-from yt_short import overlay_videos_with_blur
+from yt_short import executavel
 import os
 import logging
+import json
 
 os.system('cls')
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-video_id = 't0bdqJrVJdk' #ID do video. Somente alterar aqui
+video_id = '2lSHKP8eMmc' #ID do video. 
 relative_path = fr'C:\Users\Beffa\Documents\Python\AIShortsCreator' #Altere para a pasta do prejeto.
 
 def deletar_arquivos(pasta):
@@ -19,7 +20,7 @@ def deletar_arquivos(pasta):
         except Exception as e:
             logging.info(f"Erro ao deletar o arquivo {file_path}: {e}")
 
-def prompt_generator(transcript):
+def prompt_generator(transcript, video_id):
     last_start = transcript[-1]['start']
     div = float(last_start)/6
     dic_temp = {'content1':[],'content2':[],'content3':[],'content4':[],'content5':[],'content6':[]}
@@ -42,14 +43,18 @@ def prompt_generator(transcript):
                 "end_time": 1337.039,
                 "description": "terg 300"
                 }]}
-
-    for i in range(1, ((len(dic_temp))+1)):
-        print(f'\nPrompt {i}')
-        print('You are a ViralGPT helpful assistant. You are master at reading youtube transcripts and identifying the most Interesting and Viral Content\n')
-        print(f"This is a transcript of a video. Please identify the 3 most viral sections from the whole, make sure they are more than 40 seconds in duration, Make Sure you provide extremely accurate timestamps respond only in this format {template}. json, Here is the Transcription:\n")
-        print(dic_temp[f'content{i}'])
-        print()
-    logging.info(f'Prompts Gerados. {i} de 6')
+    
+    if not os.path.exists(fr'{relative_path}\prompts\{video_id}.json'):
+        with open(fr'{relative_path}\prompts\{video_id}.json', 'w', encoding='utf-8') as file:
+            for i in range(1, ((len(dic_temp))+1)):
+                file.write(f'\nPrompt {i}\n')
+                file.write('You are a ViralGPT helpful assistant. You are master at reading youtube transcripts and identifying the most Interesting and Viral Content\n')
+                file.write(f"This is a transcript of a video. Please identify the 3 most viral sections from the whole, make sure they are more than 40 seconds in duration, Make Sure you provide extremely accurate timestamps respond only in this format {template}. json, Here is the Transcription:\n")
+                file.write(json.dumps(dic_temp[f'content{i}'], indent=4, ensure_ascii=False))
+                file.write('\n')
+            logging.info(f'Prompts Gerados. Total: {i} de 6')
+    else:
+        logging.info(f'Prompts Existente. {video_id}')
 
 def verifica_ou_cria_pasta(past_path):
     if not os.path.exists(past_path):
@@ -68,21 +73,21 @@ logging.info('Iniciando do programa:')
 verifica_ou_cria_pasta(fr'{relative_path}\input')
 verifica_ou_cria_pasta(fr'{relative_path}\final')
 verifica_ou_cria_pasta(fr'{relative_path}\pngs')
+verifica_ou_cria_pasta(fr'{relative_path}\prompts')
 verifica_ou_cria_pasta(temp_files)
 verifica_ou_cria_pasta(json_files)
 
-cond = 1
-if cond == 0:
+#Alterar aqui para pegar os cortes. (alt + z), por padrao fica em None, coloque o prompt gerado.
+interesting_segment = {'content': [{'start_time': 70.6, 'end_time': 122.52, 'description': 'Introdução dos participantes, com destaque para as profissões e comentários humorísticos sobre a situação.'},{"start_time": 135.72,"end_time": 175.72,"description": "Johnny falando sobre sua adolescência e a pomadinha da vó."},{"start_time": 203.159,"end_time": 246.56,"description": "Vinícius discute suas experiências como garçom e os desafios com o uniforme."},{'start_time': 144.0, 'end_time': 195.799, 'description': 'História do Vitor sobre seu sonho de ser jogador de futebol, o uso de GH e a condição de bromidrose, incluindo apelidos e a música cantada por colegas.'},{"start_time": 187.519,"end_time": 234.159,"description": "Vittor fala sobre sua infância, tratamento para crescimento e bromidrose."},{'start_time': 216.319, 'end_time': 282.52, 'description': 'Relato do Milton sobre sua experiência com CC desde a infância, uso de pomada, desenvolvimento de alergia e situações engraçadas associadas.'},{"start_time": 393.36,"end_time": 442.36,"description": "Discussion about Botox and electroshock treatments for excessive sweating, with humorous exchanges about the effects and the transfer of odor to other parts of the body."},{"start_time": 477.68,"end_time": 527.6,"description": "Exploration of hyperosmia (heightened sense of smell) and its impact on daily life and work, including dealing with strong odors in a kitchen environment."},{"start_time": 600.44,"end_time": 652.92,"description": "Conversation about how people notice and react to body odor, with personal anecdotes about how others' reactions can signal the presence of an unpleasant smell."},{"start_time": 724.959,"end_time": 764.16,"description": "Discussion about the guest's relationship, infidelity, and the reactions to his hygiene."},{"start_time": 907.12,"end_time": 950.72,"description": "Conversation about personal hygiene and working conditions in a kitchen, including humorous exchanges about smell and cleanliness."},{"start_time": 974.399,"end_time": 1017.199,"description": "Detailed discussion on daily routines, hygiene practices, and the challenges faced in maintaining cleanliness in a busy lifestyle."},{"start_time": 1231.88,"end_time": 1261.0,"description": "Discussion about the experience of riding a crowded bus and dealing with unpleasant odors, leading to a humorous exchange about taking a shower before the ride."},{"start_time": 1303.76,"end_time": 1345.039,"description": "A funny story about working as a shop salesman and dealing with body odor, which affected his sales performance, followed by reactions from others."},{"start_time": 1375.48,"end_time": 1404.96,"description": "A comedic segment involving a challenge to smell someones armpit to verify body odor, with humorous commentary and reactions from the participants."},{'start_time': 1487.559,'end_time': 1531.32,'description': 'Humorous and intense moment with participants discussing and experiencing unpleasant smells, leading to laughter and reactions.'},{'start_time': 1553.08,'end_time': 1607.72,'description': 'Engaging discussion about the social consequences of bad odor, including personal anecdotes and humorous exchanges.'},{'start_time': 1643.52,'end_time': 1709.24,'description': 'Debate about whether bad odor is real or a character act, with funny and insightful commentary on the nature of body odor.'},{'start_time': 1910.48, 'end_time': 1936.48, 'description': 'Vitor reveals that he is a chef and restaurant owner, explaining his experience with bathing to remove kitchen grease.'}, {'start_time': 2001.2, 'end_time': 2029.679, 'description': 'Discussion about personal hygiene and the exaggeration of body odor issues, involving humorous and relatable anecdotes.'}, {'start_time': 2072.639, 'end_time': 2101.56, 'description': 'Introduction of a personal hygiene kit, featuring various products and a humorous demonstration.'}]}
+
+if interesting_segment == None:
     download_video(input_path, video_id)
     transcript = get_transcript(video_id)
-    prompt_generator(transcript)
+    prompt_generator(transcript, video_id)
     logging.info('Fim do programa.')
 else:
-    #Alterar aqui para pegar os cortes. (alt + z)
-    interesting_segment = {'content': [{'start_time': 0.12, 'end_time': 54.28, 'description': 'Introduction to the video and its concept, explaining the rules and the competition with a surprise for the participants.'}, {'start_time': 63.16, 'end_time': 136.84, 'description': 'Round one where participants are ranked based on appearance, with humorous and engaging interactions among the hosts.'}, {'start_time': 170.44, 'end_time': 276.919, 'description': 'Round two, where the hosts ask the participants about their areas of work and favorite restaurants, leading to interesting and funny responses.'},{"start_time": 318.32,"end_time": 379.72,"description": "Discussion about international travels, frequency of trips, and associated costs, featuring some humor and curiosity about destinations."},{"start_time": 471.84,"end_time": 526.88,"description": "Conversation about meeting Mickey at Disney, including personal emotions and reactions, transitioning into questions about household appliances with humorous elements."},{"start_time": 536.68,"end_time": 598.24,"description": "Light-hearted banter about refrigerators, the number of doors they have, and a humorous debate on the truthfulness of these claims, ending with a discussion on hobbies like football and samba."},{'start_time': 600.16, 'end_time': 641.639, 'description': 'Humorous exchange about perceptions and reactions to a montage and interactions among friends.'}, {'start_time': 688.399, 'end_time': 729.079, 'description': 'Discussion about travel expenses, affordable prices, and gratitude for last-minute deals.'}, {'start_time': 740.12, 'end_time': 780.12, 'description': 'Rapid-fire questions about financial status, personal preferences, and social habits.'},{"start_time": 969.04,"end_time": 996.399,"description": "O Léo fala sobre ser o menos abastado financeiramente e a discussão segue sobre o impacto disso no grupo."},{"start_time": 1030.36,"end_time": 1055.24,"description": "Os participantes discutem os itens mais caros que já compraram, incluindo um PC de 4000 e a obra de uma cozinha."},{"start_time": 1087.039,"end_time": 1121.039,"description": "Paloma fala sobre os itens caros que já comprou, como um iPad e um grill de dente de ouro, gerando reações surpreendidas dos outros participantes."},{'start_time': 1263.64, 'end_time': 1302.08, 'description': 'Discussion on the possibility of Gustavo earning more and the challenges of interpreting peoples financial situations.'}, {'start_time': 1343.6, 'end_time': 1394.24, 'description': 'Analysis of the potential cost of travel destinations and the expenses related to personal property improvements.'}, {'start_time': 1398.4, 'end_time': 1425.88, 'description': 'Final debate on the rankings of individuals based on perceived financial status, with a focus on Gustavo and Tulio.'},{"start_time": 1536.279,"end_time": 1572.48,"description": "Excitement and tension build as the group waits for the results of a competition, leading to a moment of celebration and relief."},{"start_time": 1643.279,"end_time": 1672.279,"description": "The group celebrates their victory and everyone is awarded R$3,000 for travel packages, creating a joyful and triumphant atmosphere."},{"start_time": 1692.2,"end_time": 1710.039,"description": "The conversation turns lively with discussions about relationships, ending with a call to action for viewers to like, subscribe, and comment."}]}
-
     parsed_content = interesting_segment["content"]
-    overlay_videos_with_blur(parsed_content, video_id)
+    executavel(video_id, parsed_content)
 
     deletar_arquivos(temp_files)
     deletar_arquivos(json_files)
--- yt_short.py
+++ yt_short.py
@@ -17,7 +17,7 @@ def enhance_video_quality(input_path, output_path):
         fr'-c:v h264_nvenc -preset fast -c:a copy {output_path}'
     )
     
-    subprocess.call(command, shell=True)
+    subprocess.run(command, shell=True, check=True)
     logging.info(f'Vídeo processado e melhorado salvo em: {output_path}')
     return output_path
 
@@ -28,7 +28,7 @@ def remove_black_borders(video_path, output_path):
     left = 0  
     top = original_height // 2.8  
     right = original_width  
-    bottom = original_height * 2.5 // 4 
+    bottom = original_height * 2.6 // 4 
     
     cropped_video = video.crop(x1=left, y1=top, x2=right, y2=bottom)
     cropped_video.write_videofile(output_path, codec="libx264", fps=video.fps)
@@ -36,10 +36,13 @@ def remove_black_borders(video_path, output_path):
 
 def segment_video(response, video_id):
     output_files = []
+    output_files_full = []
+
     for i, segment in enumerate(response):
         start_time = math.floor(float(segment.get("start_time", 0)))
         end_time = math.ceil(float(segment.get("end_time", 0))) + 2
         output_file = fr"AIShortsCreator/output/{video_id}_output{str(i).zfill(3)}.mp4"
+        output_file_full = fr"AIShortsCreator/output/{video_id}_full_output{str(i).zfill(3)}.mp4"
         input_file = fr"C:\Users\Beffa\Documents\Python\AIShortsCreator\input\{video_id}_input_video.mp4"
         
         command = (
@@ -47,56 +50,51 @@ def segment_video(response, video_id):
             fr'scale=w=1080:h=-1:force_original_aspect_ratio=decrease,pad=1080:1920:(ow-iw)/2:(oh-ih)/2 '
             fr'-c:v h264_nvenc -preset fast -c:a copy {output_file}'
         )
-
-        subprocess.call(command, shell=True)
-        logging.info(f'Segmento video {i} gerado: {output_file}')
-        output_files.append(output_file)
-    return output_files
-
-def segment_video_full(response, video_id):
-    output_files = []
-    for i, segment in enumerate(response):
-        start_time = math.floor(float(segment.get("start_time", 0)))
-        end_time = math.ceil(float(segment.get("end_time", 0))) + 2
-        output_file = fr"AIShortsCreator/output/{video_id}_full_output{str(i).zfill(3)}.mp4"
-        input_file = fr"C:\Users\Beffa\Documents\Python\AIShortsCreator\input\{video_id}_input_video.mp4"
-        
-        command = (
+        command_full = (
             fr'ffmpeg -i {input_file} -ss {start_time} -to {end_time} -vf '
             fr'scale=1080:1920:force_original_aspect_ratio=increase,crop=1080:1920 '
-            fr'-c:v h264_nvenc -preset fast -c:a copy {output_file}'
+            fr'-c:v h264_nvenc -preset fast -c:a copy {output_file_full}'
         )
 
-        subprocess.call(command, shell=True)
-        logging.info(f'Segmento de vídeo {i} gerado: {output_file}')
+        subprocess.run(command, shell=True, check=True)
+        subprocess.run(command_full, shell=True, check=True)
+
+        logging.info(f'Segmento video {i} gerado: {output_file}')
+        logging.info(f'Segmento video {i} gerado: {output_file_full}')
+
         output_files.append(output_file)
-    return output_files
+        output_files_full.append(output_file_full)
 
-def overlay_videos_with_blur(response, video_id):
-    segmented_videos = segment_video(response, video_id)
-    full_segmented_videos = segment_video_full(response, video_id)
-    
-    for i, (segmented, full_segmented) in enumerate(zip(segmented_videos, full_segmented_videos)):
-        output_path = fr"AIShortsCreator/output/{video_id}_final_output{str(i).zfill(3)}.mp4"
+def overlay_videos_with_blur(input_segmented_videos, input_full_segmented_videos, video_id, i):  
+    output_path = fr"AIShortsCreator/output/{video_id}_final_output{str(i).zfill(3)}.mp4"
+    no_border_segmented_path = remove_black_borders(input_segmented_videos, fr"AIShortsCreator/output/{video_id}_no_border_output{str(i).zfill(3)}.mp4")
 
-        time.sleep(2)
-        no_border_segmented_path = remove_black_borders(segmented, fr"AIShortsCreator/output/{video_id}_no_border_output{str(i).zfill(3)}.mp4")
+    command = (
+        fr'ffmpeg -i {no_border_segmented_path} -i {input_full_segmented_videos} -filter_complex '
+        fr'"[1:v]boxblur=luma_radius=60:luma_power=1[blurred];'
+        fr'[blurred][0:v]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" '
+        fr'-c:v h264_nvenc -preset fast -c:a copy {output_path}'
+    )
 
-        command = (
-            fr'ffmpeg -i {no_border_segmented_path} -i {full_segmented} -filter_complex '
-            fr'"[1:v]boxblur=luma_radius=60:luma_power=1[blurred];'
-            fr'[blurred][0:v]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" '
-            fr'-c:v h264_nvenc -preset fast -c:a copy {output_path}'
-        )
+    subprocess.run(command, shell=True, check=True)
+    logging.info(f'Vídeo gerado com sobreposição e desfoque: {output_path}')
+    return output_path
 
-        subprocess.call(command, shell=True)
-        logging.info(f'Vídeo gerado com sobreposição e desfoque: {output_path}')
-        
+def executavel(video_id, response):
+    segment_video(response, video_id)
+    for i, segment in enumerate(response):
+        input_segmented_videos = fr'AIShortsCreator/output/{video_id}_output{str(i).zfill(3)}.mp4'
+        input_full_segmented_videos = fr"AIShortsCreator/output/{video_id}_full_output{str(i).zfill(3)}.mp4"
+        output_path = overlay_videos_with_blur(input_segmented_videos, input_full_segmented_videos, video_id, i)
+
         enhanced_output_path = fr"AIShortsCreator/output/{video_id}_posprocess_output{str(i).zfill(3)}.mp4"
         enhance_video_quality(output_path, enhanced_output_path)
+        
         watermark_output_path = fr"AIShortsCreator/output/{video_id}_watermark_output{str(i).zfill(3)}.mp4"
         add_watermark(enhanced_output_path,watermark_output_path)
+        
         text_output_path = fr"AIShortsCreator/output/{video_id}_text_{str(i).zfill(3)}.mp4"
         add_text(watermark_output_path, text_output_path)
+        
         final_output_path = fr"AIShortsCreator/final/{video_id}_final_{str(i).zfill(3)}.mp4"
         subtitle(text_output_path, final_output_path, video_id, i)
